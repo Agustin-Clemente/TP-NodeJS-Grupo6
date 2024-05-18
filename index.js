ab9c5b4897f6c41e8838b3af3f2d79202a2ef098
Agush <a_clemente@live.com.ar>
2024-05-17 23:53:01 -0300

    Nuevas cards y validacion de form

---
+++ index.js
@@ -0,0 +1,15 @@
+function habilitar() {
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let dni = document.getElementById("dni");
+    let destino = document.getElementById("destino");
+    let cantidad = document.getElementById("cantidad");
+    let pago = document.getElementById("pago");
+    let botonPedir = document.getElementById("pedir");
+  
+    if (nombre.value === '' || apellido.value === '' || dni.value === '' || destino.value === '' || cantidad.value === '' || pago.value === '') {
+        botonPedir.disabled = true;
+    } else {
+        botonPedir.disabled = false;
+    }
+  }
